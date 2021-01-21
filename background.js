24bd43c2239251554b89e96f2162e848c28c1815
Benzene <benzeneworks@gmail.com>
2021-01-22 00:28:13 +0900

    pageActionに変更,URLで有効無効を切り替え

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {
+                    urlMatches: 'www\.amazon\.co\.jp.*(/dp|/gp/product)/[A-Z0-9]{10}'
+                }
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
