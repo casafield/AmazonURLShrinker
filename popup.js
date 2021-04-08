5045806074026c560a6b97d70c157af3e0fc7e91
Benzene <benzeneworks@gmail.com>
2021-04-08 23:42:00 +0900

    replace修正

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   // アクティブなタブのURLを取得
   const regex = /(www\.amazon\.co\.jp).*(\/dp|\/gp\/product)(\/[A-Z0-9]{10}).*/;
-  let shortenedURL = tabs[0].url.replace(regex, '$1$2$3');
-  document.querySelector("#url").value = shortenedURL;
+  document.querySelector("#url").value = tabs[0].url.replace(regex, '$1$2$3');
 });
