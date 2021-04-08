85e980c2f07d0c10787be085a22934443aec4de8
Benzene <benzeneworks@gmail.com>
2021-04-08 23:36:31 +0900

    URLを短縮するreplaceを追加

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   // アクティブなタブのURLを取得
-  document.querySelector("#url").value = tabs[0].url;
+  const regex = /(www\.amazon\.co\.jp).*(\/dp|\/gp\/product)(\/[A-Z0-9]{10}).*/;
+  let shortenedURL = tabs[0].url.replace(regex, '$1$2$3');
+  document.querySelector("#url").value = shortenedURL;
 });
